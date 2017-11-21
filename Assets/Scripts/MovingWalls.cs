376ed2e8af392690f587bda5a8f1675adbc5fb1e
samilesma <samilesma@gmail.com>
2017-11-21 15:44:07 +0100

    I see some changes
    fdsaf

---
+++ Assets/Scripts/MovingWalls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWalls : MonoBehaviour {
+
+    public float min = 2f;
+    public float max = 3f;
+    // Use this for initialization
+    void Start()
+    {
+
+        min = transform.position.z;
+        max = transform.position.z + 3;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+
+    }
+}
