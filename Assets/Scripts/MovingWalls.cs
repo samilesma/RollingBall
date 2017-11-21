6e9c6f5a6dd015242f312459a6eb657b3de3dc83
Mikfor <mik_f@outlook.dk>
2017-11-21 16:54:52 +0100

    Har ændret i MovingWalls
    Man kan nu indtaste char x,y eller z, også bevæger væggen sig i den
    retning man ønsker istedet for man skal lave et script til hver gang.

--- Assets/Scripts/MovingWalls.cs
+++ Assets/Scripts/MovingWalls.cs
@@ -7,19 +7,36 @@ public class MovingWalls : MonoBehaviour {
     public float min = 2f;
     public float max = 3f;
     public int distance = 3;
+	public char pos;
     // Use this for initialization
     void Start()
     {
+		if (pos == 'y') {
+			min = transform.position.y;
+			max = transform.position.y + distance;
+		} else if (pos == 'x') {
+			min = transform.position.x;
+			max = transform.position.x + distance;
+		} else if (pos == 'z') {
+			min = transform.position.z;
+			max = transform.position.z + distance;
+		}
 
-        min = transform.position.z;
-        max = transform.position.z + distance;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+		if (pos == 'y') {
+			transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.z);
+		} 
+		else if (pos == 'x') {
+			transform.position = new Vector3( Mathf.PingPong(Time.time * 2, max - min) + min , transform.position.y, transform.position.z);
+		} 
+		else if (pos == 'z') {
+			transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+		}
     }
 
 
