fd9de9a7e8f79c2935f744765d6cbd39e06a7cf2
samilesma <samilesma@gmail.com>
2017-11-21 16:34:13 +0100

    Moving Walls

--- Assets/Scripts/MovingWalls.cs
+++ Assets/Scripts/MovingWalls.cs
@@ -6,12 +6,13 @@ public class MovingWalls : MonoBehaviour {
 
     public float min = 2f;
     public float max = 3f;
+    public int distance = 3;
     // Use this for initialization
     void Start()
     {
 
         min = transform.position.z;
-        max = transform.position.z + 3;
+        max = transform.position.z + distance;
 
     }
 
@@ -19,6 +20,7 @@ public class MovingWalls : MonoBehaviour {
     void Update()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
-
     }
+
+
 }
