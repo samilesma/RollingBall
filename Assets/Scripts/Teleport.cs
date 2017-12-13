409998d9826416f1f5eeee9afacb95c1afcc587f
samilesma <samilesma@gmail.com>
2017-12-13 11:37:53 +0100

    Portal

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+    public Transform spawnPoint;
+    public GameObject tpObject;
+    private float timer = 1.0f;
+    private bool justCameOut = false;
+    // Use this for initialization
+
+
+    
+    void OnTriggerEnter(Collider collider)
+    {
+            tpObject.transform.position = spawnPoint.position;
+    }
+}
