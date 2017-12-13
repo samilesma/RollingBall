f78faf32b978d2c27bfbad38559d405d31b8cd47
samilesma <samilesma@gmail.com>
2017-12-13 14:23:07 +0100

    Count Text virker til forskellige screen sizes

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
-public class CountDown : MonoBehaviour {
+public class CountDown : MonoBehaviour
+{
     //public float timeLeft = 30.0f;
     public int timeLeft = 10;
     public Text countDownText;
     public bool gameEnded = false;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         StartCoroutine("LoseTime");
-	}
+    }
 
     void Update()
     {
         // timeLeft -= Time.deltaTime;
 
-        countDownText.text = ("Time Left:" + timeLeft+ "s");
+        countDownText.text = ("Time Left:" + timeLeft + "s");
         if (timeLeft <= 5)
         {
             countDownText.color = new Color(229.0f / 255.0f, 45.0f / 255.0f, 33.0f / 255.0f);
