4f645b1519e9f3556d42a2569762ffba0f45336d
ahma0942 <ahmad.almajdi@yahoo.dk>
2017-12-13 14:22:48 +0100

    jkl;]

---
+++ Assets/Scripts/WallJump.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJump : MonoBehaviour {
+    private bool isWalled = false;
+    private bool spacedown = false;
+    public int jump = 8;
+    public bool jumper = false;
+    public enum Force {X,Z};
+    public Force direction = Force.X;
+    public bool negativeDirection = false;
+    private PlayerController playerScript;
+
+    void Start () {
+        GameObject player = GameObject.Find("Player");
+        playerScript = player.GetComponent<PlayerController>();
+    }
+
+    private void FixedUpdate()
+    {
+        int x=direction==Force.X?negativeDirection?-jump: jump : 0, y=jump, z= direction == Force.Z ? negativeDirection ? -jump : jump : 0;
+        if ((spacedown || jumper) && isWalled) playerScript.rb.AddForce(x,y,z, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) spacedown = true;
+        else if (Input.GetKeyUp(KeyCode.Space)) spacedown = false;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player") isWalled = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player") isWalled = false;
+    }
+
+
+}
