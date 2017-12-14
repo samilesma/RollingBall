803d1f5f13af2beaeb642b9024b1d1693247f4da
ahma0942 <ahmad.almajdi@yahoo.dk>
2017-12-14 15:39:09 +0100

    hjkl;'

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MoveDown : MonoBehaviour {
     private bool onit = false;
+    public enum Directions {X,Y,Z};
+    public Directions Direction = Directions.X;
+    public bool Negate = false;
+    public GameObject Object;
     
 	// Use this for initialization
 	void Start () {
@@ -12,7 +16,15 @@ public class MoveDown : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (onit) transform.position = new Vector3(transform.position.x, transform.position.y-0.01f, transform.position.z);
+        if (onit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
+            Object.transform.position = new Vector3(
+                Object.transform.position.x + (Direction == Directions.X ? 0.1f : 0),
+                Object.transform.position.y + (Direction == Directions.Y ? 0.1f : 0),
+                Object.transform.position.z + (Direction == Directions.Z ? 0.1f : 0)
+            );
+        }
 	}
 
     void OnCollisionEnter(Collision collision)
